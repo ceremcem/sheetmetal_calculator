c5a973861becf626419fcebd03b21813718e6e98
Cerem Cem ASLAN <cem@aktos.io>
2020-06-10 05:44:03 +0300

    refactored folder structure, switched to Cirrus CSS

--- docs/build.sh
+++ docs/build.sh
@@ -3,12 +3,11 @@ set -eu -o pipefail
 set_dir(){ _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; }; set_dir
 safe_source () { source $1; set_dir; }
 
-pug_file="$_dir/index.pug"
+pug_file="$_dir/app/index.pug"
 if [[ -f "$pug_file" ]]; then
-    pug -w --pretty "$pug_file" & 
+    pug -w --pretty "$pug_file" -o ./ &
 else
     echo "No $(basename $pug_file) found."
 fi
-
 
 wait 
